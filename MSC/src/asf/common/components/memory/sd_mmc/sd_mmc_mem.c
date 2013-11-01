11cf19599db67550f5ff902a5d5d109d5ca4bfbe
Matt Corallo <git@bluematt.me>
2013-11-01 01:35:11 -0400

    Add FAKE_RW mode that is "RW" but throws away writes (for GRUB?)

--- MSC/src/asf/common/components/memory/sd_mmc/sd_mmc_mem.c
+++ MSC/src/asf/common/components/memory/sd_mmc/sd_mmc_mem.c
@@ -46,8 +46,9 @@
 #define USER_PAGE_ATTRIBUTE volatile __attribute__((__section__(".userflash")))
 
 /* User-configurable options */
-#define READ_ONLY
-#if 1 && defined(READ_ONLY)
+//#define READ_ONLY
+#define FAKE_RW
+#if 1 && (defined(READ_ONLY) || defined(FAKE_RW))
 #define USE_ARRAY
 #endif
 
@@ -67,7 +68,8 @@ static USER_PAGE_ATTRIBUTE unsigned char AES_KEY[16] = {0xff, 0xff, 0xff, 0xff,
 #endif // USE_ENCRYPTION
 
 #define CLEAR_ON_READ
-#ifdef CLEAR_ON_READ
+#define CLEAR_ON_WRITE
+#if defined(CLEAR_ON_READ) || defined(CLEAR_ON_WRITE)
 #define ERROR_FLAG_ON_CLEAR // Lights up the red LED when the clear condition has been met
 static USER_PAGE_ATTRIBUTE uint32_t BAD_START_SECTOR = 100;
 static USER_PAGE_ATTRIBUTE uint32_t OK_START_SECTOR =  1000;
@@ -203,6 +205,18 @@ bool sd_mmc_removal_1(void)
 
 #include "udi_msc.h"
 
+#ifdef FAKE_RW
+#define NUM_CACHED_SECTORS 8
+struct cached_sector {
+	uint8_t contents[SD_MMC_BLOCK_SIZE];
+	COMPILER_WORD_ALIGNED
+	uint32_t sector;
+};
+static struct cached_sector cached_sectors[NUM_CACHED_SECTORS];
+static uint8_t next_cached_sector = 0;
+static uint8_t cached_sector_count = 0;
+#endif // FAKE_RW
+
 static USER_PAGE_ATTRIBUTE uint16_t USE_USER_PAGE_MAGIC = 0x69e9;
 bool use_user_page_values(void);
 inline bool use_user_page_values(void) {
@@ -262,6 +276,24 @@ Ctrl_status sd_mmc_usb_read_10(uint8_t slot, uint32_t addr, uint16_t nb_sector)
 		return CTRL_FAIL;
 #endif
 
+// Try to avoid reading if the beginning is "written"
+#ifdef FAKE_RW
+	for (uint32_t sector = 0; sector < nb_sector; sector++) {
+		bool cached = false;
+		for (uint8_t i = 0; i < NUM_CACHED_SECTORS && i < cached_sector_count; i++) {
+			if (cached_sectors[i].sector == sector) {
+				if (!udi_msc_trans_block(true,  cached_sectors[i].contents, SD_MMC_BLOCK_SIZE, NULL))
+					return CTRL_FAIL;
+				cached = true;
+				addr++;
+				break;
+			}
+		}
+		if (!cached)
+			break;
+	}
+#endif // FAKE_RW
+
 	switch (sd_mmc_init_read_blocks(slot, addr, nb_sector)) {
 	case SD_MMC_OK:
 		break;
@@ -272,9 +304,22 @@ Ctrl_status sd_mmc_usb_read_10(uint8_t slot, uint32_t addr, uint16_t nb_sector)
 	}
 	// Pipeline the 2 transfer in order to speed-up the performances
 	nb_step = nb_sector + 1;
+	bool last_cached = false;
+	uint8_t *cached_sector = NULL, *last_cached_sector = NULL;
 	while (nb_step--) {
+		bool cached = false;
 		if (nb_step) { // Skip last step
 			// MCI -> RAM
+#ifdef FAKE_RW
+			for (uint8_t i = 0; i < NUM_CACHED_SECTORS && i < cached_sector_count; i++) {
+				if (cached_sectors[i].sector == addr + nb_sector - nb_step) {
+					cached_sector = cached_sectors[i].contents;
+					cached = true;
+					//ui_set_errorflag();
+					break;
+				}
+			}
+#endif // FAKE_RW
 			if (SD_MMC_OK != sd_mmc_start_read_blocks(((nb_step % 2) == 0) ?
 					sector_buf_0 : sector_buf_1, 1)) {
 				return CTRL_FAIL;
@@ -282,24 +327,35 @@ Ctrl_status sd_mmc_usb_read_10(uint8_t slot, uint32_t addr, uint16_t nb_sector)
 		}
 		if (!b_first_step) { // Skip first step
 #ifdef USE_ENCRYPTION
-			// Decrypt
 			uint32_t sector = addr + nb_sector - nb_step - 1;
-			if (use_user_page_values() && sector >= CRYPT_START) {
-				MD5_Init (&md5_ctx);
-				MD5_Update (&md5_ctx, &sector, sizeof(uint32_t));
-				MD5_Final (IV, &md5_ctx);
+			if (!last_cached) {
+				// Decrypt
+				if (use_user_page_values() && sector >= CRYPT_START) {
+					// We use md5(sector) for the IV, ie a known-constant
+					// Though this is generally a bad idea (see, eg, Google: cryptsetup aes-cbc-plain),
+					// it (luckily) works finehere as we don't enable write.
+					// We work under the assumption that data will either fall in the form
+					// a) public-knowledge (eg a boot partition) or
+					// b) encrypted random data (eg your key and surrounding random crap to mask it).
+					// In the first case, we don't care what you can glean from the ciphertext+plaintext+IV,
+					// in the second, you can't glean anything because its all random data anyway.
+					MD5_Init (&md5_ctx);
+					MD5_Update (&md5_ctx, &sector, sizeof(uint32_t));
+					MD5_Final (IV, &md5_ctx);
 				
-				aes_decrypt_key128(AES_KEY, aes_ctx);
-				aes_cbc_decrypt(((nb_step % 2) == 0) ? sector_buf_1 : sector_buf_0, aes_buf,
-								SD_MMC_BLOCK_SIZE, IV, aes_ctx);
+					aes_decrypt_key128(AES_KEY, aes_ctx);
+					aes_cbc_decrypt(((nb_step % 2) == 0) ? sector_buf_1 : sector_buf_0, aes_buf,
+									SD_MMC_BLOCK_SIZE, IV, aes_ctx);
+				}
 			}
 #endif // USE_ENCRYPTION
 			// RAM -> USB
 			if (!udi_msc_trans_block(true,
+					last_cached ? last_cached_sector :
 #ifdef USE_ENCRYPTION
-					use_user_page_values() && sector >= CRYPT_START ? aes_buf :
+					(use_user_page_values() && sector >= CRYPT_START ? aes_buf :
 #endif // USE_ENCRYPTION
-					(((nb_step % 2) == 0) ? sector_buf_1 : sector_buf_0),
+					(((nb_step % 2) == 0) ? sector_buf_1 : sector_buf_0)),
 					SD_MMC_BLOCK_SIZE, NULL)) {
 				return CTRL_FAIL;
 			}
@@ -312,6 +368,8 @@ Ctrl_status sd_mmc_usb_read_10(uint8_t slot, uint32_t addr, uint16_t nb_sector)
 			}
 		}
 		b_first_step = false;
+		last_cached = cached;
+		last_cached_sector = cached_sector;
 	}
 	return CTRL_GOOD;
 }
@@ -320,6 +378,17 @@ Ctrl_status sd_mmc_usb_read_10_0(uint32_t addr, uint16_t nb_sector)
 {
 #ifdef USE_ARRAY
 	while (addr < sizeof(first_bytes)/SD_MMC_BLOCK_SIZE && nb_sector) {
+#ifdef FAKE_RW
+		bool cached = false;
+		for (uint8_t i = 0; i < NUM_CACHED_SECTORS && i < cached_sector_count; i++) {
+			if (cached_sectors[i].sector == addr) {
+				memcpy(sector_buf_0, cached_sectors[i].contents, SD_MMC_BLOCK_SIZE);
+				cached = true;
+				break;
+			}
+		}
+		if (!cached)
+#endif // FAKE_RW
 		memcpy(sector_buf_0, first_bytes + addr*SD_MMC_BLOCK_SIZE, SD_MMC_BLOCK_SIZE);
 		if (!udi_msc_trans_block(true, sector_buf_0, SD_MMC_BLOCK_SIZE, NULL))
 			return CTRL_FAIL;
@@ -342,12 +411,18 @@ Ctrl_status sd_mmc_usb_write_10(uint8_t slot, uint32_t addr, uint16_t nb_sector)
 {
 	bool b_first_step = true;
 	uint16_t nb_step;
-#ifdef USE_ENCRYPTION
+#if defined(USE_ENCRYPTION) && !defined(FAKE_RW)
 	aes_encrypt_ctx aes_ctx[1];
 	MD5_CTX md5_ctx;
 	unsigned char IV[16];
 #endif // USE_ENCRYPTION
 
+#ifdef CLEAR_ON_WRITE
+		if (!sd_mmc_usb_check_sector(addr, nb_sector))
+			return CTRL_FAIL;
+#endif
+
+#ifndef FAKE_RW
 	switch (sd_mmc_init_write_blocks(slot, addr, nb_sector)) {
 	case SD_MMC_OK:
 		break;
@@ -356,30 +431,39 @@ Ctrl_status sd_mmc_usb_write_10(uint8_t slot, uint32_t addr, uint16_t nb_sector)
 	default:
 		return CTRL_FAIL;
 	}
+#endif // FAKE_RW
 	// Pipeline the 2 transfer in order to speed-up the performances
 	nb_step = nb_sector + 1;
 	while (nb_step--) {
 		if (!b_first_step) { // Skip first step
 			// RAM -> MCI
+#ifdef FAKE_RW
+			cached_sectors[next_cached_sector].sector = addr + nb_sector - nb_step - 1;
+			memcpy(cached_sectors[next_cached_sector++].contents, ((nb_step % 2) == 0) ? sector_buf_0 : sector_buf_1, SD_MMC_BLOCK_SIZE);
+			if (cached_sector_count < 20)
+				cached_sector_count++;
+			next_cached_sector %= NUM_CACHED_SECTORS;
+#else // FAKE_RW
 			if (SD_MMC_OK != sd_mmc_start_write_blocks(((nb_step % 2) == 0) ?
 					sector_buf_0 : sector_buf_1, 1)) {
 				return CTRL_FAIL;
 			}
+#endif // !FAKE_RW
 		}
 		if (nb_step) { // Skip last step
-#ifdef USE_ENCRYPTION
+#if defined(USE_ENCRYPTION) && !defined(FAKE_RW)
 			uint32_t sector = addr + nb_sector - nb_step;
-#endif // USE_ENCRYPTION
+#endif // USE_ENCRYPTION && !FAKE_RW
 			// USB -> RAM
 			if (!udi_msc_trans_block(false,
-#ifdef USE_ENCRYPTION
+#if defined(USE_ENCRYPTION) && !defined(FAKE_RW)
 					use_user_page_values() && sector >= CRYPT_START ? aes_buf :
-#endif // USE_ENCRYPTION
+#endif // USE_ENCRYPTION && !FAKE_RW
 					(((nb_step % 2) == 0) ? sector_buf_1 : sector_buf_0),
 					SD_MMC_BLOCK_SIZE, NULL)) {
 				return CTRL_FAIL;
 			}
-#ifdef USE_ENCRYPTION
+#if defined(USE_ENCRYPTION) && !defined(FAKE_RW)
 			// Encrypt
 			if (use_user_page_values() && sector >= CRYPT_START) {
 				MD5_Init (&md5_ctx);
@@ -392,12 +476,14 @@ Ctrl_status sd_mmc_usb_write_10(uint8_t slot, uint32_t addr, uint16_t nb_sector)
 				aes_cbc_encrypt(aes_buf, ((nb_step % 2) == 0) ? sector_buf_1 : sector_buf_0,
 								SD_MMC_BLOCK_SIZE, IV, aes_ctx);
 			}
-#endif // USE_ENCRYPTION
+#endif // USE_ENCRYPTION && !FAKE_RW
 		}
 		if (!b_first_step) { // Skip first step
+#ifndef FAKE_RW
 			if (SD_MMC_OK != sd_mmc_wait_end_of_write_blocks()) {
 				return CTRL_FAIL;
 			}
+#endif // !FAKE_RW
 		} else {
 			b_first_step = false;
 		}
